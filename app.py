009115d3e0b6dcb56cec7b8c74eacef98053bbf2
Elon <elonmusk@gmail.com>
2022-02-12 02:57:03 -0600

    hjello

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def tests():
         ).join(Projects, Running.project == Projects.id, 
         ).join(Status, Results.status == Status.id).filter(Running.uuid == request.json.get('uuid')).all()])
     if(request.json.get('type') == "testcases"):
-        return jsonify([i.as_dict() for i in TestRunner.query.filter(TestRunner.project == request.json.get('proj_id'))])
+        return jsonify([i.as_dict() for i in TestRunner.query.filter(TestRunner.project == request.json.get('proj_id')).all()])
     if(request.json.get('type') == "running"):
         return jsonify([i.as_dict() for i in Running.query.all()])
     if(request.json.get("type") == "users"):
