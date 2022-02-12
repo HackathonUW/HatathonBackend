f3bd5ef38ca000a4d891d76ae4fac7aadcdf4c5b
Elon <elonmusk@gmail.com>
2022-02-11 21:30:48 -0600

    tests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, jsonify
+from flask import Flask,request, jsonify, make_response
 import gunicorn
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, MetaData, Table, Enum
@@ -6,7 +6,7 @@ from sqlalchemy.types import TEXT
 import os
 from flask_cors import CORS
 from MySQLdb import _mysql
-
+import uuid
 app = Flask(__name__)
 connstr =  "mysql://etlfzuiqep3x9epw:rm0aadwhwg8876si@z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bvfo3h955t68zhoz"
 app.config['SQLALCHEMY_DATABASE_URI'] = connstr
@@ -20,6 +20,16 @@ app.config['UPLOAD_FOLDER'] = '/static'
 meta = MetaData()   
 meta.bind= engine
 
+class Projects(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    tests = db.relationship('TestRunner', backref='Projects', lazy=True)
+    course = db.Column(db.String(255))
+    section = db.Column(db.String(255))
+    lastupdated = db.Column(db.DATETIME)
+    prof = db.Column(db.String(255))
+    runs = db.relationship('Running', backref='Projects', lazy=True)
+
 class TestRunner(db.Model):
     pid = db.Column(db.Integer,nullable=False, primary_key=True)
     ratings = db.Column(db.Integer)
@@ -29,12 +39,37 @@ class TestRunner(db.Model):
     pre = db.Column(db.String(255), nullable = False)
     post = db.Column(db.String(255), nullable = False)
     command = db.Column(db.String(255), nullable = False)
+    project = db.Column(db.ForeignKey('projects.id'), nullable = False)
+    runs = db.relationship('Running', backref='Results', lazy=True)
+
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 class Users(db.Model):
     email = db.Column(db.String(255), nullable = False, primary_key = True)
     name = db.Column(db.Integer, nullable = False)
-    tests = db.Column(db.TEXT, nullable = False)
+    runs = db.relationship('Running', backref='Projects', lazy=True)
+
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+class Running(db.Model):
+    pid = db.Column(db.Integer, nullable=False, primary_key=True)
+    email = db.Column(db.ForeignKey('users.email'), nullable=False)
+    project = db.Column(db.ForeignKey('projects.id'), nullable=False)
+    
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class Results(db.Model):
+    uuid = db.Column(db.String(255), primary_key=True, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    tests = db.Column(db.ForeignKey('test_runner.pid'), nullable=False)
+    time = db.Column(db.DATETIME, nullable=False)
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class Status(db.Model):
+    id = db.Column(db.String(255), nullable =False, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
@@ -46,39 +81,48 @@ def create():
         file.save(os.path.join(app.config["UPLOAD_FOLDER"]), file.filename)
         return jsonify({"path" : os.path.join(app.config["UPLOAD_FOLDER"])})
     if(request.json.get('type', None) == "user"):
-        db.session.add(Users(email = request.json.get('email', "N/A"), name = request.json.get('name', "N/A"), tests= ""))
+        user = Users(email = request.json.get('email', "N/A"), name = request.json.get('name', "N/A"), tests= "")
+        db.session.add(user)
         db.session.commit()
-        return jsonify({"error" : False})
+        return jsonify({"user" : False})
     if(request.json.get('type', None) == "testcase"):
-        db.session.add(TestRunner(
+        if(not request.json.get("projectid")):
+            return jsonify({"error":True})
+        test = TestRunner(
             ratings = request.json.get('rating', None), author = request.json.get('author', 'John Doe'), name = request.json.get('name', 'N/A'),
             lastupdated = request.json.get('date', None), pre = request.json.get('pre', ""), post = request.json.get('post', ""), 
-            command = request.json.get("command", "") ))
-        return jsonify({"error" : False})
+            command = request.json.get("command", ""), project = request.json.get("projectid", 1)) 
+        db.session.add(test)
+        db.session.flush()
+        pid = test.pid
+        db.session.commit()
+        return jsonify({"test_id" : pid})
+    if(request.json.get('type', None) == "project"):
+        project = Projects(name=request.json.get("name"),course=request.json.get("course"),section=request.json.get("section", "")
+        ,lastupdated=request.json.get("lastupdated"), prof = request.json.get("prof",""))
+        db.session.add(project)
+        db.session.flush()
+        pid = project.id
+        db.session.commit()
+        return jsonify({"proj_id":pid})
     return jsonify({"error" : True})
 
 
 
-@app.route('/tests', methods=['POST'])
+@app.route('/get', methods=['POST'])
 def tests():
-    print(request.json.get('name',None))
-    if(request.json.get('name',None) == 'all'):
-        return jsonify([i.as_dict() for i in db.session.query(TestRunner).all()])
-    if(request.json.get('name', None)):
-        return jsonify([i.as_dict() for i in db.session.query(TestRunner).filter(name=request.json.get("name")).first()])
-    if(request.json.get('id', None)):
-        return jsonify([i.as_dict() for i in db.session.query(TestRunner).filter(pid = request.json.get("id").first())])
+    if(request.json.get('type') == "project"):
+
     return jsonify({"error":False})
-@app.route('/users', methods = ['POST'])
-def users():
-    if(request.json.get('name', None) != None):
-        tests = Users.query.filter_by(email=request.json.get('email')).first()["tests"].split(" ")
-    return jsonify({"error" : False})
 
 
+@app.route('/new', methods = ['POST'])
+def new():
+    return jsonify({"uuid": uuid.uuid4()})
 if(__name__ == "__main__"):
+    db.create_all()
     '''
     with engine.connect() as con:
-        print(con.execute("SELECT * FROM information_schema.tables").fetchall())
+        con.execute("INSERT INTO status(id,name) VALUES (0,'pass'), (1, 'fail'), (2,'not started'), (3,'running'),(4,'inconclusive') ")
     '''
     app.run()
